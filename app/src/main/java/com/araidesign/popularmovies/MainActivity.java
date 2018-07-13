ef4bbf28e8a3dbf02d95602b7385cced3ddbc4d8
cgarai <chris@araidesign.com>
2018-07-12 19:27:05 -0700

    Added test for internet connectivity, but not sure how to test it.

--- app/src/main/java/com/araidesign/popularmovies/MainActivity.java
+++ app/src/main/java/com/araidesign/popularmovies/MainActivity.java
@@ -1,6 +1,10 @@
 package com.araidesign.popularmovies;
 
+import android.arch.core.BuildConfig;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,34 +28,35 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements MovieAdapter.ItemClickListener {
 
     private RecyclerView mRecyclerView;
-    private  MovieAdapter mMovieAdapter;
+    private MovieAdapter mMovieAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static ArrayList<MovieData> allMovieData = new ArrayList<>();
 
-    private  String api_key;
+    private String api_key;
 
     private static final int NUM_MOVIES = 20;
     private Toast mToast;
     ProgressBar mLoadingIndicator;
     private static boolean onAppStart = true;
 
-//    public MainActivity(){
-//
-//        String api_key = getString(R.string.my_API_key);
-////        makePopularMovieSearch();
-//    }
-
+    private Context context = this;
+    private NetworkInfo activeNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TODO 1:  Currently set up to use the BuildConfig method of hiding the API_Key from github.
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        activeNetwork = cm.getActiveNetworkInfo();
+
+//        TODO 1:  Currently set up to use the no_github.xml method of hiding the API_Key from github.  I couldn't get the suggested method of using Build.Config to work
 //        If no_github.xml contains the API key then use:
-//        api_key = getString(R.string.my_API_key);
+        api_key = getString(R.string.my_API_key);
 //        Otherwise use:
-        api_key = BuildConfig.my_API_KEY;
+//        api_key = BuildConfig.my_API_KEY;
 
         mRecyclerView = findViewById(R.id.movie_recycler_view);
 
@@ -61,9 +66,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
         mMovieAdapter = new MovieAdapter(this, allMovieData);
         mRecyclerView.setAdapter(mMovieAdapter);
 
-        mLoadingIndicator =  findViewById(R.id.pb_load_indicator);
+        mLoadingIndicator = findViewById(R.id.pb_load_indicator);
 
-        if (onAppStart){
+        if (onAppStart) {
             onAppStart = false;
             makePopularMovieSearch();
 
@@ -73,11 +78,11 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
     }
 
 
-    private void launchDetailActivity(int position){
+    private void launchDetailActivity(int position) {
         String movieJson;
 
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_POSITION,position);
+        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
         intent.putExtra(DetailActivity.MOVIE_DATA, allMovieData.get(position));
         startActivity(intent);
 
@@ -93,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 //        int itemClicked = item.getItemId();
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_popular:
                 makePopularMovieSearch();
                 break;
@@ -121,22 +126,47 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
 
     void makePopularMovieSearch() {
         allMovieData.clear();
-        URL url = NetworkUtils.buildTMDBQueryURL(api_key,getString(R.string.popular_movie_search));
+        URL url = NetworkUtils.buildTMDBQueryURL(api_key, getString(R.string.popular_movie_search));
 
-        new MovieSearchTask().execute(url);
+//        TODO 4:  Checking on network availability before starting MovieSearchTask  I'm not sure how to test it
+        if (activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting()) {
+            new MovieSearchTask().execute(url);
+        }
+        else {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                String toastMessage = "What?! No Internet?";
+                mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+                mToast.show();
+
+        }
     }
 
-    void makeTopRatedMovieSearch(){
+    void makeTopRatedMovieSearch() {
         allMovieData.clear();
-        URL url = NetworkUtils.buildTMDBQueryURL(api_key,getString(R.string.top_rated_movie_search));
-        new MovieSearchTask().execute(url);
+        URL url = NetworkUtils.buildTMDBQueryURL(api_key, getString(R.string.top_rated_movie_search));
+        if (activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting()) {
+            new MovieSearchTask().execute(url);
+        }
+        else {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                String toastMessage = "What?! No Internet?";
+                mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+                mToast.show();
+
+        }
 
     }
 
 //    TODO 3:  I get a warning that AsyncTask should be static, but  then mLoadingIndicator and mMovieAdapter need to be static, but that causes warnings as well
 //    Warning:(149, 19) This AsyncTask class should be static or leaks might occur (com.araidesign.popularmovies.MainActivity.MovieSearchTask)
 
-    public  class MovieSearchTask extends AsyncTask<URL, Void, ArrayList<MovieData>> {
+    public class MovieSearchTask extends AsyncTask<URL, Void, ArrayList<MovieData>> {
 
         @Override
         protected void onPreExecute() {
@@ -146,7 +176,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
 
 
         @Override
-        protected  ArrayList<MovieData> doInBackground(URL... url) {
+        protected ArrayList<MovieData> doInBackground(URL... url) {
             String movieListJSON;
             ArrayList<MovieData> movieDataFromJson = new ArrayList<>();
             try {
@@ -166,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Item
         }
 
         @Override
-        protected void onPostExecute(ArrayList<MovieData> movieData){
+        protected void onPostExecute(ArrayList<MovieData> movieData) {
             mLoadingIndicator.setVisibility(View.INVISIBLE);
 
             allMovieData.addAll(movieData);
